19991f0ebb31f93cdcda8432cfb5e5a0eabf6487
theunbound <the.bentusi@gmail.com>
2021-11-13 03:37:36 +0100

    add melpa stuff

--- skewer-stylus.el
+++ skewer-stylus.el
@@ -2,6 +2,11 @@
 
 ;; This is free and unencumbered software released into the public domain.
 
+;; Author: Kristoffer Levin Hansen <kristoffer@levinhansen.dk>
+;; Keywords: languages, tools
+;; Version: 1.0.0
+;; Package-Requires: ((skewer-mode "1.8.0"))
+
 ;;; Commentary:
 
 ;; This minor mode provides functionality for Stylus like plain skewer
